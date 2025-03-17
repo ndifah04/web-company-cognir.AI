3ee6f6630d60029be6918d58cc3c8a2e2e49dc74
mamanwhide <abdulvivo52@gmail.com>
2025-03-17 11:20:47 +0000

    config-fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['cognir.ai']
+  },
+  server: {
+    host: true,
+    port: 4173
+  }
 })
+
