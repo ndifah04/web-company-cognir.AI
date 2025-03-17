3178e05cdee1e7751de9a8f5acde43b5f8ef9aa5
Nurman2021 <nurman.awaluddin@student.unismuh.ac.id>
2025-03-17 15:17:44 +0800

    update env model

--- src/components/gltf-viewer/index.jsx
+++ src/components/gltf-viewer/index.jsx
@@ -5,15 +5,13 @@ import { Environment, } from '@react-three/drei';
 
 const GLBViewer = () => {
     return (
-        // <div className='hero-canvas'>
         <Canvas className='w-100' >
-            <Environment preset="park" />
+            <Environment preset="city" />
             <ambientLight intensity={0.5} />
             <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
             <pointLight position={[-10, -10, -10]} intensity={0.5} />
             <Model url="/model/face-v1.glb" />
         </Canvas>
-        // </div>
 
     );
 };
